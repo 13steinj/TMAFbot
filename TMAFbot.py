f153b6bf860ccb001f18a7bba0ed3e28779821f5
13steinj <13steinj@users.noreply.github.com>
2015-12-05 12:10:49 -0500

    Reduce redundancy

--- TMAFbot.py
+++ TMAFbot.py
@@ -1,50 +1,37 @@
-import time
 import praw
-import random
-import re
-import urllib2
-import sys
-import traceback
-import requests
-from pprint import pprint
-import sys
-
 
 # creates sets where the posts it has already counted are stored.
 r = praw.Reddit('TMAFbot by /u/peoplma')
 r.login("XXXXXXXXX","XXXXXXXXX")
 already_done = set()
 subName = 'TellMeAFact'
-deleted = str('[deleted]')
+deleted = '[deleted]'
 nl = "\n"
-subreddit_posts = r.get_content(url='https://www.reddit.com/r/TellMeAFact/top/', params=None, limit=10, place_holder=None, root_field=u'data', thing_field=u'children', after_field=u'after', object_filter=None)
+s = r.get_subreddit(subName)
+subreddit_posts = s.get_top_from_week(limit=10)
 for post in subreddit_posts:
-		print post.score
-		print post.title
-		filename='TMAFbot/'+'TMAFbot.txt'
+		print(post.score))
+		print(post.title
+		filename='TMAFbot/TMAFbot.txt'
 		post_score = str(post.score)
-		post_title = str('**' + post.title + '**')
+		post_title = '**{0}**'.format(post.title)
 		obj=open(filename, 'ab+')
-		strpost_score = str('*(' + post_score + ')*' + ' ')
-		strpost_title = str(post_title)
+		strpost_score = '*({0})*'.format(post_score)
 		obj.write(nl + '***' + nl + nl + strpost_score)
-		obj.write(strpost_title + nl + nl + '***' + nl)
+		obj.write(post_title + nl + nl + '***' + nl)
 		obj.close()
 		submission = r.get_submission(submission_id = post.id)
 		for comment in submission.comments:
-			commentstr = str(comment.body)
+			commentstr = comment.body
 			comment_score = str(comment.score)
 			if commentstr != deleted and comment.score >= 3:
-				print comment.body
-				filename='TMAFbot/'+'TMAFbot.txt'
+				print(comment.body)
+				filename='TMAFbot/TMAFbot.txt'
 				obj=open(filename, 'ab+')
-				strcomment_score = str('*(' + comment_score + ')*' + ' ')
-				strcommentstr = str(commentstr)
+				strcomment_score = '*({0})*'.format(comment_score)
 				obj.write(strcomment_score)
-				obj.write(strcommentstr + nl + nl)
+				obj.write(commentstr + nl + nl)
 				obj.close()
-		#pprint(vars(post))
-		data= {'user-agent':'archive by /u/healdb and /u/peoplma'}
 		#manually grabbing this file is much faster than loading the individual json files of every single comment, as this json provides all of it
 		if post.id not in already_done:
 			#Create a folder called TMAFbot before running the script
